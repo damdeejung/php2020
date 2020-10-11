c8cf33201ea75ea7521ac0930981d0ce6e1a7bdb
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-11 07:05:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,4 +25,16 @@ function notify_message($message,$token){
 }
 //https://havespirit.blogspot.com/2017/02/line-notify-php.html
 //https://medium.com/@nattaponsirikamonnet/%E0%B8%A1%E0%B8%B2%E0%B8%A5%E0%B8%AD%E0%B8%87-line-notify-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-%E0%B8%9E%E0%B8%B7%E0%B9%89%E0%B8%99%E0%B8%90%E0%B8%B2%E0%B8%99-65a7fc83d97f
+//<?php
+
+require_once './vendor/autoload.php';
+
+$token = '3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk';
+$ln = new KS\Line\LineNotify($token);
+
+$text = ' '; // Line Notify บังคับให้ใส่ข้อความ แต่อยากส่งแแต่รูปภาพเลยใส่ space ไว้
+$image_path = 'https://i.ytimg.com/vi/zxFqW-QIsUI/maxresdefault.jpg'; //Line notify allow only jpeg and png file
+$ln->send($text, $image_path);
+
+
 ?>
