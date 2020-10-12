43773411b98971f1814b759becf39d6c164e6691
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-12 14:29:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,20 @@ function notify_message($message,$stickerPkg,$stickerId,$token){
      );
      $context = stream_context_create($headerOptions);
      $result = file_get_contents(LINE_API,FALSE,$context);
+     
+     $queryData = http_build_query($queryData,'','&');    
+     $headerOptions = array(
+         'http'=>array(
+             'method'=>'POST',
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Bearer 3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk\r\n"
+                       ."Content-Length: ".strlen($queryData)."\r\n",
+             'content' => $queryData
+         ),
+     );
+     $context = stream_context_create($headerOptions);
+     $result = file_get_contents(LINE_API,FALSE,$context);
+     
      $res = json_decode($result);
   return $res;
  }
