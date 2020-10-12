a62ccf36495f0459cb492c98fa73648bfc9e718c
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-12 14:45:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php
 define('LINE_API',"https://notify-api.line.me/api/notify");
-//$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
 $str = "hello World"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
 $stickerPkg = 1; //stickerPackageId
 $stickerId = 106; //stickerId
@@ -18,27 +18,13 @@ function notify_message($message,$stickerPkg,$stickerId,$token){
          'http'=>array(
              'method'=>'POST',
              'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
-                 ."Authorization: Bearer 3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk\r\n"
+                 ."Authorization: Bearer ".$token."\r\n"
                        ."Content-Length: ".strlen($queryData)."\r\n",
              'content' => $queryData
          ),
      );
      $context = stream_context_create($headerOptions);
-     $result = file_get_contents(LINE_API,FALSE,$context);
-     
-     $queryData = http_build_query($queryData,'','&');
-     $headerOptions = array(
-         'http'=>array(
-             'method'=>'POST',
-             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
-                 ."Authorization: Bearer rU5QQMxtA8VE3gWzGmyE9R4GYA3kjIQrArcU0UIKevq\r\n"
-                       ."Content-Length: ".strlen($queryData)."\r\n",
-             'content' => $queryData
-         ),
-     );
-     $context = stream_context_create($headerOptions);
-     $result = file_get_contents(LINE_API,FALSE,$context);
-     
+     $result = file_get_contents(LINE_API,FALSE,$context);     
      $res = json_decode($result);
   return $res;
  }
