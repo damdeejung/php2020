f238b7849ad043e713f2dcdc2c860633e9845e90
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-12 13:27:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-
-<?php
-
-/*-------------line notify----------------------*/
+/*-------------line notify----------------------
 $line_api = 'https://notify-api.line.me/api/notify';
 $access_token = '3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk';
 $message = 'test Hello World';    //text max 1,000 charecter
@@ -23,7 +20,7 @@ echo '<pre>';
 print_r($result);
 echo '</pre>';
 }
-/*-------------line noti----------------------*/
+/*-------------line noti----------------------
 function send_notify_message($line_api, $access_token, $message_data){
 $headers = array('Method: POST', 'Content-type: multipart/form-data', 'Authorization: Bearer '.$access_token );
 $ch = curl_init();
@@ -45,4 +42,36 @@ $return_array = json_decode($result, true);
 curl_close($ch);
 return $return_array;
 }
+------------------------------------------------------------------------*/
+<?php
+define('LINE_API',"https://notify-api.line.me/api/notify");
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$str = "Hello World"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+$stickerPkg = 2; //stickerPackageId
+$stickerId = 34; //stickerId
+ 
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+function notify_message($message,$stickerPkg,$stickerId,$token){
+     $queryData = array(
+      'message' => $message,
+      'stickerPackageId'=>$stickerPkg,
+      'stickerId'=>$stickerId
+     );
+     $queryData = http_build_query($queryData,'','&');
+     $headerOptions = array(
+         'http'=>array(
+             'method'=>'POST',
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Bearer ".$token."\r\n"
+                       ."Content-Length: ".strlen($queryData)."\r\n",
+             'content' => $queryData
+         ),
+     );
+     $context = stream_context_create($headerOptions);
+     $result = file_get_contents(LINE_API,FALSE,$context);
+     $res = json_decode($result);
+  return $res;
+ }
+
 ?>
