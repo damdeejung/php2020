34b42d7449050b61aeb7532e3d97a7dfe64ff58c
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-19 14:22:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 
-<?php
 /*
 define('LINE_API',"https://notify-api.line.me/api/notify");
 $token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
@@ -67,46 +66,34 @@ function notify_message($params, $token) {
   return $res;
 }
 */
-define('LINE_API', "https://notify-api.line.me/api/notify");
-//$x = 2;
-//while($x >= 1) {
-//$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้         
- //    while($x <= 2) {
-          $token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
-  //        $x++;          
-          $params = array(
-            "message"        => "อรุนสวัสดิ์ครับ", //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
-            "stickerPkg"     => 1, //stickerPackageId
-            "stickerId"      => 2, //stickerId
-            "imageThumbnail" => "https://media.giphy.com/media/GEC49lPULGaoE/giphy.gif", // max size 240x240px JPEG
-            "imageFullsize"  => "https://media.giphy.com/media/GEC49lPULGaoE/giphy.gif", //max size 1024x1024px JPEG
-          );
-          $res = notify_message($params, $token);
-          // print_r($res);
+<?php
+define('LINE_API',"https://notify-api.line.me/api/notify");
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$str = "hello World"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+$stickerPkg = 1; //stickerPackageId
+$stickerId = 106; //stickerId
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+function notify_message($message,$stickerPkg,$stickerId,$token){
+     $queryData = array(
+      'message' => $message,
+      'stickerPackageId'=>$stickerPkg,
+      'stickerId'=>$stickerId
+     );
+     $queryData = http_build_query($queryData,'','&');
+     $headerOptions = array(
+         'http'=>array(
+             'method'=>'POST',
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Bearer ".$token."\r\n"
+                       ."Content-Length: ".strlen($queryData)."\r\n",
+             'content' => $queryData
+         ),
+     );
+     $context = stream_context_create($headerOptions);
+     $result = file_get_contents(LINE_API,FALSE,$context);     
+     $res = json_decode($result);
+  return $res;
+ }
 
-          function notify_message($params, $token) {
-            $queryData = array(
-              'message'          => $params["message"],
-              'stickerPackageId' => $params["stickerPkg"],
-              'stickerId'        => $params["stickerId"],
-              'imageThumbnail'   => $params["imageThumbnail"],
-              'imageFullsize'    => $params["imageFullsize"],
-            );
-            $queryData = http_build_query($queryData, ' ', '&');
-            $headerOptions = array(
-              'http' => array(
-                'method'  => 'POST',
-                'header'  => "Content-Type: application/x-www-form-urlencoded\r\n"
-                . "Authorization: Bearer " . $token . "\r\n"
-                . "Content-Length: " . strlen($queryData) . "\r\n",
-                'content' => $queryData,
-              ),
-            );
-            $context = stream_context_create($headerOptions);
-            $result = file_get_contents(LINE_API, FALSE, $context);
-            $res = json_decode($result);
-            return $res;
-          }
-    // }
-   //}
 ?>
