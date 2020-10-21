0ece9bc9d293ef6cbc40fc5d80c8da9f9c2a9c2d
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-21 21:34:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<?php
+define('LINE_API',"https://notify-api.line.me/api/notify");
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$str = "hello World"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+$stickerPkg = 1; //stickerPackageId
+$stickerId = 106; //stickerId
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+function notify_message($message,$stickerPkg,$stickerId,$token){
+     $queryData = array(
+      'message' => $message,
+      'stickerPackageId'=>$stickerPkg,
+      'stickerId'=>$stickerId
+     );
+     $queryData = http_build_query($queryData,'','&');
+     $headerOptions = array(
+         'http'=>array(
+             'method'=>'POST',
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Bearer ".$token."\r\n"
+                       ."Content-Length: ".strlen($queryData)."\r\n",
+             'content' => $queryData
+         ),
+     );
+     $context = stream_context_create($headerOptions);
+     $result = file_get_contents(LINE_API,FALSE,$context);     
+     $res = json_decode($result);
+  return $res;
+ }
+
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+notify_message(); // call the function
+
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+notify_message(); // call the function
+
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+notify_message(); // call the function
+
+
+
+?>
 
 /*
 define('LINE_API',"https://notify-api.line.me/api/notify");
@@ -65,8 +113,9 @@ function notify_message($params, $token) {
   $res = json_decode($result);
   return $res;
 }
-*/
-<?php
+
+
+
 define('LINE_API', "https://notify-api.line.me/api/notify");
 $token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
 $params = array(
@@ -108,4 +157,5 @@ $res = notify_message($params, $token);
 // print_r($res);
 notify_message(); // call the function
 
-?>
+
+*/
