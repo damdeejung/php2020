4c41f40aeec4bf0bf08e9cd92cfc0ea74c30779e
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-21 22:08:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,27 @@ $stickerPkg = 1; //stickerPackageId
 $stickerId = 106; //stickerId
 $res = notify_message($str,$stickerPkg,$stickerId,$token);
 print_r($res);
-notify_message(); // call the function
+function notify_message($message,$stickerPkg,$stickerId,$token){
+     $queryData = array(
+      'message' => $message,
+      'stickerPackageId'=>$stickerPkg,
+      'stickerId'=>$stickerId
+     );
+     $queryData = http_build_query($queryData,'','&');
+     $headerOptions = array(
+         'http'=>array(
+             'method'=>'POST',
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Bearer ".$token."\r\n"
+                       ."Content-Length: ".strlen($queryData)."\r\n",
+             'content' => $queryData
+         ),
+     );
+     $context = stream_context_create($headerOptions);
+     $result = file_get_contents(LINE_API,FALSE,$context);     
+     $res = json_decode($result);
+  return $res;
+ }
 
 define('LINE_API',"https://notify-api.line.me/api/notify");
 $token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
@@ -44,7 +64,28 @@ $stickerPkg = 1; //stickerPackageId
 $stickerId = 106; //stickerId
 $res = notify_message($str,$stickerPkg,$stickerId,$token);
 print_r($res);
-notify_message(); // call the function
+function notify_message($message,$stickerPkg,$stickerId,$token){
+     $queryData = array(
+      'message' => $message,
+      'stickerPackageId'=>$stickerPkg,
+      'stickerId'=>$stickerId
+     );
+     $queryData = http_build_query($queryData,'','&');
+     $headerOptions = array(
+         'http'=>array(
+             'method'=>'POST',
+             'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                 ."Authorization: Bearer ".$token."\r\n"
+                       ."Content-Length: ".strlen($queryData)."\r\n",
+             'content' => $queryData
+         ),
+     );
+     $context = stream_context_create($headerOptions);
+     $result = file_get_contents(LINE_API,FALSE,$context);     
+     $res = json_decode($result);
+  return $res;
+ }
+
 
 
 ?>
