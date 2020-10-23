a272a21a841e3979467ceb8990a7f17ae7211c90
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-23 18:26:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,12 @@ function notify_message($message,$stickerPkg,$stickerId,$token){
    $res = json_decode($result);
 }     
   return $res;  
- //}
+ //----------
+$token = "3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk"; //ใส่Token ที่copy เอาไว้
+$res = notify_message($str,$stickerPkg,$stickerId,$token);
+print_r($res);
+notify_message(); // call the function
+return $res;  
 
 ?>
 
