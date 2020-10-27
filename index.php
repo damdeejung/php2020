85fe3b44ceee9c96a46b995ae9ce107774f2be5d
damdeejung <38566142+damdeejung@users.noreply.github.com>
2020-10-27 17:01:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ $access_token = '3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk';
 $str = 'ทดสอบข้อความ';    //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
 $image_thumbnail_url = 'https://media.giphy.com/media/JAnseEvaGvviN9enPX/giphy.gif';  // ขนาดสูงสุด 240×240px JPEG
 $image_fullsize_url = 'https://media.giphy.com/media/JAnseEvaGvviN9enPX/giphy.gif';  // ขนาดสูงสุด 1024×1024px JPEG
-$sticker_package_id = 1;  // Package ID ของสติกเกอร์
-$sticker_id = 2;    // ID ของสติกเกอร์
+//$sticker_package_id = 1;  // Package ID ของสติกเกอร์
+//$sticker_id = 2;    // ID ของสติกเกอร์
 
 $message_data = array(
  'message' => $str,
  'imageThumbnail' => $image_thumbnail_url,
  'imageFullsize' => $image_fullsize_url,
- 'stickerPackageId' => $sticker_package_id,
- 'stickerId' => $sticker_id
+ //'stickerPackageId' => $sticker_package_id,
+ //'stickerId' => $sticker_id
 );
 
 $result = send_notify_message($line_api, $access_token, $message_data);
@@ -50,15 +50,6 @@ $result = send_notify_message($line_api, $access_token, $message_data);
 print_r($result);
 send_notify_message();
 
-$access_token = '3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk';
-$result = send_notify_message($line_api, $access_token, $message_data);
-print_r($result);
-send_notify_message();
-
-$access_token = '3ACDH8LYP69SBzA171EZs8Vg4Edlh9i5ZBVfBmSUhMk';
-$result = send_notify_message($line_api, $access_token, $message_data);
-print_r($result);
-send_notify_message();
 
 
 ?>
